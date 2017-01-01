e90fc9cc7ed509ccf347c0c3cda776bfb6f4deee
danny <danny.h.williams@hotmail.com>
2017-01-01 22:00:41 +0000

    formatting

--- imgdetective/hash_generator.py
+++ imgdetective/hash_generator.py
@@ -1,31 +1,31 @@
+"""
+Module containing hash generating functions for images.
+"""
 from __future__ import absolute_import, division, print_function
 
-# import six
 import imagehash
 from PIL import Image
 
 
-# def get_hash_method(hashmethod):
-#     hash_methods = {
-#         'ahash': imagehash.average_hash,
-#         'phash': imagehash.phash,
-#         'dhash': imagehash.dhash,
-#         'whash-haar': imagehash.whash,
-#         'whash-db4': lambda img: imagehash.whash(img, mode='db4')
-#     }
-#     return hash_methods[hashmethod]
+def generate_hashes_for_image(img_path):
+    """
+    This function is used to generate various hashes for an image and return a
+    dictionary which contains each of these hashes.
 
+    args:
+        img_path - path to the image which we will generate the hashes for.
+    returns:
+        hashes - Dict containing boolean arrarys representing hashes of the
+        supplied image.
+    """
+    print ("Generating hash for: " + img_path)
+    img = Image.open(img_path)
 
-def generate_hashes_for_image(imgPath):
+    hashes = {}
+    hashes['ahash'] = imagehash.average_hash(img, hash_size=8)
+    hashes['phash'] = imagehash.phash(img, hash_size=8)
+    hashes['dhash'] = imagehash.dhash(img, hash_size=8)
+    hashes['whash'] = imagehash.whash(img, hash_size=8)
+    hashes['whashDb4'] = imagehash.whash(img, mode='db4')
 
-    print ("Generating hash for: " + imgPath)
-    img = Image.open(imgPath)
-
-    data = {}
-    data['ahash'] = imagehash.average_hash(img, hash_size=8)
-    data['phash'] = imagehash.phash(img, hash_size=8)
-    data['dhash'] = imagehash.dhash(img, hash_size=8)
-    data['whash'] = imagehash.whash(img, hash_size=8)
-    data['whashDb4'] = imagehash.whash(img, mode='db4')
-    print(data)
-    return data
+    return hashes
--- lambda.py
+++ lambda.py
@@ -1,6 +1,11 @@
+"""
+This module contains the handler method for a lambda function that will
+generate hashes for an image uploaded to S3.
+"""
+
 from __future__ import print_function
 
-import os
+import json
 import urllib
 
 import boto3
@@ -11,19 +16,33 @@ S3_CLIENT = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    # print("Received event: " + json.dumps(event, indent=2))
+    """
+    Handler for an AWS lambda function which is triggered by an object being
+    created in an S3 bucket, hashes will be generated for that image and
+    printed.
+    
+    Args:
+        event: dict containing information on the create object event
+        context: LambdaContext object with information (unused)
+    Returns:
+        Null
+    """
+    print("Received event: " + json.dumps(event, indent=2))
 
-    # Get the object from the event and show its content type
+    # get the s3 object from the event object
     bucket = event['Records'][0]['s3']['bucket']['name']
+    # get s3 object key
     key = urllib.unquote_plus(event[
             'Records'][0]['s3']['object']['key'].encode('utf8'))
     try:
-        # response = s3.get_object(Bucket=bucket, Key=key)
-        # print("CONTENT TYPE: " + response['ContentType'])
+        # download file into /tmp so we can work with it
         S3_CLIENT.download_file(Bucket=bucket, Key=key,
                                 Filename=TEMP_FILE_PATH)
-        hash_generator.generate_hashes_for_image(TEMP_FILE_PATH)
-        print(os.listdir("/tmp"))
+
+        # generate hashes for image and log them
+        hashes = hash_generator.generate_hashes_for_image(TEMP_FILE_PATH)
+        print(hashes)
+
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist \
